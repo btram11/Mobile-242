ae38c5f239080c0f8ec3765c0ad108af055c697d
QuanCters <quan.nguyen2212804@hcmut.edu.vn>
2025-03-02 22:06:10 +0700

    # This is a combination of 5 commits. # This is the 1st commit message:
    #16 - update Prisma and Gitignore
    
    # This is the commit message #2:
    
    #16 - update package
    
    # This is the commit message #3:
    
    #16 - Add close redis to server.js
    
    # This is the commit message #4:
    
    #16 - update redis init file
    
    # This is the commit message #5:
    
    #16 - update repositories based on schema

--- backend/src/dbs/repositories/user.repo.js
+++ backend/src/dbs/repositories/user.repo.js
@@ -1,73 +1,86 @@
 // const db = require("../init.postgres");
-const { PrismaClient } = require('@prisma/client');
+const { PrismaClient } = require("@prisma/client");
 
 const prisma = new PrismaClient();
 
 const InsertUser = async (user) => {
-  const result = await prisma.users.create(user)
-    .catch((error) => { console.error(error); throw error; });
+  const result = await prisma.user.create(user).catch((error) => {
+    console.error(error);
+    throw error;
+  });
   return result;
-}
+};
 
 const getAllUsers = async () => {
-  const result = await prisma.users.findMany().catch((error) => { console.error(error); throw error; });
+  const result = await prisma.users.findMany().catch((error) => {
+    console.error(error);
+    throw error;
+  });
   return result;
-}
+};
 
 const getUserByEmail = async (email) => {
-  const result = await prisma.users.findUnique({
-    where: {
-      email: email,
-    },
-  }).catch((error) => {
-    console.error(error);
-    throw error;
-  });
+  const result = await prisma.user
+    .findUnique({
+      where: {
+        email: email,
+      },
+    })
+    .catch((error) => {
+      console.error(error);
+      throw error;
+    });
   return result;
-}
+};
 
 const removeToken = async (access_token) => {
-  const result = await prisma.users.update({
-    where: {
-      access_token: access_token,
-    },
-    data: {
-      access_token: "",
-    },
-  }).catch((error) => {
-    console.error(error);
-    throw error;
-  });
-}
+  const result = await prisma.user
+    .update({
+      where: {
+        access_token: access_token,
+      },
+      data: {
+        access_token: "",
+      },
+    })
+    .catch((error) => {
+      console.error(error);
+      throw error;
+    });
+};
 const setTokenById = async (access_token, id) => {
-  const result = await prisma.users.update({
-    where: {
-      id: id,
-    },
-    data: {
-      access_token: access_token,
-    },
-  }).catch((error) => {
-    console.error(error);
-    throw error;
-  });
+  const result = await prisma.user
+    .update({
+      where: {
+        user_id: id,
+      },
+      data: {
+        access_token: access_token,
+      },
+    })
+    .catch((error) => {
+      console.error(error);
+      throw error;
+    });
   return result;
-}
+};
 const updatePasswordById = async (new_password, salt, id) => {
-  const result = await prisma.users.update({
-    where: {
-      id: id,
-    },
-    data: {
-      password_hash: new_password,
-      salt: salt,
-    },
-  }).catch((error) => {
-    console.error(error);
-    throw error;
-  });
+  const result = await prisma.user
+    .update({
+      where: {
+        user_id: id,
+      },
+      data: {
+        password_hash: new_password,
+        salt: salt,
+      },
+    })
+    .catch((error) => {
+      console.error(error);
+      throw error;
+    });
   return result;
-}
+};
 
 module.exports = {
   getUserByEmail,
