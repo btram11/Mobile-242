a3f3c518ff7aec6b56087bf7c7c5363eff8329db
kng1259 <khoinguyenanlac1@gmail.com>
2025-03-12 23:14:38 +0700

    update seeds post merge

--- backend/prisma/seed.js
+++ backend/prisma/seed.js
@@ -2,14 +2,7 @@ const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
 
 const main = async () => {
-  const tables = [
-    "user",
-    "provider",
-    "database_book",
-    "listed_book",
-    "is_leased",
-    "is_sold",
-  ];
+  const tables = ["user", "provider", "database_book", "listed_book"];
   for (const table of tables.slice().reverse()) {
     await prisma[table].deleteMany();
   }
