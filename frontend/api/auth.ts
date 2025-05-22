7ce783a017c0616544dd20f5f0da02be84ace2cc
btram11 <misa11012004@gmail.com>
2025-05-22 23:40:20 +0700

    test

--- frontend/api/auth.ts
+++ frontend/api/auth.ts
@@ -4,24 +4,26 @@ const BaseUrl = process.env.EXPO_PUBLIC_API_URL + "/access";
 
 export const login = async (email: string, password: string) => {
   console.log("here");
-  const response = await fetch(`${BaseUrl}/user/login`, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-      // CLIENT_ID: "x-client-id",
-      // AUTHORIZATION: "authorization",
-    },
-    body: JSON.stringify({
-      email: email,
-      password: password,
-    }),
-  });
-  console.log(response);
-  if (!response.ok) throw new HttpError(response.statusText, response.status);
+  // const response = await fetch(`${BaseUrl}/user/login`, {
+  //   method: "POST",
+  //   headers: {
+  //     "Content-Type": "application/json",
+  //     // CLIENT_ID: "x-client-id",
+  //     // AUTHORIZATION: "authorization",
+  //   },
+  //   body: JSON.stringify({
+  //     email: email,
+  //     password: password,
+  //   }),
+  // });
+  // console.log(response);
+  // if (!response.ok) throw new HttpError(response.statusText, response.status);
 
-  const result = await response.json();
-  console.log(result);
-  if (result.status !== 200) throw new HttpError(result.message, result.status);
+  // const result = await response.json();
+  // console.log(result);
+  // if (result.status !== 200) throw new HttpError(result.message, result.status);
 
-  return result;
+  // return result;
+
+  return { access_token: "token", userId: "userId" }; // Mocked response
 };
